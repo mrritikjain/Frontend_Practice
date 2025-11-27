dbd24af26c4b5c27c029c8e3cd3ecb1c1554b7e9
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2025-11-27 22:23:06 +0530

    Question 2  with solution

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 /* Question 1 - Write a program to check whether a given number is odd or even using . */
 
-let num =5;
+let num = 5;
 
-if (num%2 === 0){
+if (num % 2 === 0) {
   console.log("number is Even");
-}
-else{
+} else {
   console.log("number is odd");
 }
+
+/* Question 2 - Take a student's marks as input and print their grade using conditions:
+90-100: A
+80-89: B
+70-79: C
+60-69: D
+< 60: F
+ */
+
+let marks = prompt("Enter Your Marks");
+marks = Number(marks);
+
+let grade;
+
+if (marks >= 90 && marks <= 100) {
+  grade = "A";
+}
+else if(marks >= 80 && marks <= 89){
+  grade ="B";
+} 
+else if(marks >= 70 && marks <= 79){
+  grade ="C";
+}
+else if(marks >= 60 && marks <=69){
+  grade ="D";
+}
+else if(marks >= 90 && marks <= 100){
+  grade ="E";
+}
+else if(marks < 60){
+  grade ="F";
+}
+else {
+  grade = "Invalid Grade";
+}
+
+console.log("Your grade is :", grade);
