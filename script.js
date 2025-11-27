84878d4c684f26f93f412e9b28c725e94bd48dd1
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2025-11-27 22:08:08 +0530

    Question 1 with solution

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Question 1 - Write a program to check whether a given number is odd or even using . */
+
+let num =5;
+
+if (num%2 === 0){
+  console.log("number is Even");
+}
+else{
+  console.log("number is odd");
+}
