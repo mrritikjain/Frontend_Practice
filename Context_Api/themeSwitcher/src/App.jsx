ba02df3d5d243d413b29eed918f46a1c558ea51e
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2026-07-02 18:27:30 +0530

    theme switcher with contect api is created for practicing context api

---
+++ Context_Api/themeSwitcher/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "./Context/Themecontext.jsx";
+import "./App.css";
+
+const App = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  
+  return (
+    <div className={`app-container ${theme}`}>
+      <div className="card">
+        <h2>Theme Switcher</h2>
+        <div className="theme-status">
+          Current Theme: <span className="theme-badge">{theme}</span>
+        </div>
+        <button className="toggle-btn" onClick={toggleTheme}>
+          Switch to {theme === "light" ? "dark" : "light"} Mode
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
+
